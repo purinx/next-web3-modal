7b3e2a0289b2af74c1d38b51eba8041a8535b0b6
higherkingpud <eddix.ibanez@outlook.jp>
2024-04-06 14:36:03 +0900

    fix error

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
+  // c.f https://github.com/WalletConnect/web3modal/issues/1408
+  webpack: (config) => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    return config;
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/libs/ui/Web3ModalButton.tsx
+++ src/libs/ui/Web3ModalButton.tsx
@@ -1,7 +1,29 @@
 'use client';
 
-type Props = {};
+import { useWeb3Modal } from '@web3modal/wagmi/react';
+import { useEffect } from 'react';
+import { useAccount } from 'wagmi';
 
 export const Web3ModalButton = () => {
-  return <w3m-button />;
+  const { open } = useWeb3Modal();
+  const { isConnected, chain } = useAccount();
+
+  useEffect(() => {
+    if (isConnected && chain) {
+      window.alert(`Successfully connected to ${chain?.name}`);
+    }
+  }, [isConnected, chain]);
+
+  return isConnected ? (
+    <w3m-account-button />
+  ) : (
+    <button
+      type="button"
+      onClick={() => {
+        open();
+      }}
+    >
+      Connect
+    </button>
+  );
 };
